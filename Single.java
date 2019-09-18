fbf24dc180395fd0412cb0317302e1c21fd0503a
zxf1115 <2426623758@qq.com>
2019-09-18 20:06:12 +0800

    新增一个Single.java

---
+++ Single.java
@@ -0,0 +1,7 @@
+class Single{
+     private static Single INSTANCE=new Single();
+     private Single£¨£©{}
+     public static Single getInstance(){
+           return INSTANCE;
+              }
+}
