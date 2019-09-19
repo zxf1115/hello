a9bd41984e749ddcae37bff2303f8fca5e01ff8e
zxf1115 <2426623758@qq.com>
2019-09-19 14:45:58 +0800

    新增一个HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,3 @@
+public class HelloWorld {
+     public static void main (String [] args) {
+        System.out.println("这是第一个Java程序!");}}
--- Single.java
+++
@@ -1,7 +0,0 @@
-class Single{
-     private static Single INSTANCE=new Single();
-     private Single£¨£©{}
-     public static Single getInstance(){
-           return INSTANCE;
-              }
-}
